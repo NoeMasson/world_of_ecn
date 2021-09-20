bd200abaa3e28c883ee4a2ec6c5fd935ee16df8e
Valentin Molina <valentin@molina.pro>
2021-09-20 15:48:34 +0200

    add javadoc and default name to Personnage.

--- Personnage.java
+++ Personnage.java
@@ -74,17 +74,17 @@ public class Personnage {
 
     /**
      * Personnage constructor which specify all the attributes.
-     * @param nom
-     * @param ptV
-     * @param ptM
-     * @param pA
-     * @param pP
-     * @param pM
-     * @param rM
-     * @param dA
-     * @param dM
-     * @param distMax
-     * @param pos 
+     * @param nom Name of the character.
+     * @param ptV Number of health point of the character
+     * @param ptM Number of mana point of the character.
+     * @param pA Percentage corresponding to the success of an attack.
+     * @param pP Percentage corresponding to the success of a parade
+     * @param pM Percentage corresponding to the success of a magical attack.
+     * @param rM Percentage corresponding to the resistance to magical attack.
+     * @param dA The amount of damage that inflict the character.
+     * @param dM The amount of magical damage that inflict the character.
+     * @param distMax The range at which the character can access.
+     * @param pos The position of the character on the map.
      */
     public Personnage(String nom, int ptV, int ptM, int pA, 
             int pP, int pM, int rM, int dA, int dM, int distMax, Point2D pos)
@@ -130,6 +130,7 @@ public class Personnage {
      */
     public Personnage()
     {
+        nom = "Perso";
         this.pos = new Point2D();
     }
     
