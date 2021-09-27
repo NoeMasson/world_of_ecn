925b0b3a13373742644b38ce8f7366c814c4a245
Noe Masson <noemasson@laposte.net>
2021-09-27 18:10:34 +0200

    Updated TestSeance4

--- TestSeance4.java
+++ TestSeance4.java
@@ -14,15 +14,41 @@ public class TestSeance4 {
     
     public static void main(String args[])
     {
+        /** 
+         * Variables pour la définition du monde
+         * 
+         * */
+        int nbProta;
+        int nbObj;
+        int sizeMap;
+        
+        nbProta = 10;
+        nbObj = 5; 
+        sizeMap = 20;
+        
+        /**
+         * Variables temporelles
+         * */
         float debut;
         float fin;
         
+        
+        
+        World testingWorld = new World(nbProta, nbObj, sizeMap);
+        
+        testingWorld.afficheWorld();
+               
+        
+        
+        
+        /**
+         * Tests de durée des différentes Collections
+         */
         debut = System.nanoTime();
         
         // insérer test de durée ici
-        
-        fin = System.nanoTime();
-        
+                        
+        fin = System.nanoTime();                   
         System.out.println("Cette opération a duré "+(fin-debut));
         
           
