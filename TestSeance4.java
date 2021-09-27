eafcb46c0c0348fcec6d428fbdb73ef74010383b
Noe Masson <noemasson@laposte.net>
2021-09-27 17:25:02 +0200

    ajout du fichier de test pour le TP 4

---
+++ TestSeance4.java
@@ -0,0 +1,32 @@
+package org.centrale.projet.objet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Valentin Molina
+ * @author Noe Masson
+ */
+public class TestSeance4 {
+    
+    public static void main(String args[])
+    {
+        float debut;
+        float fin;
+        
+        debut = System.nanoTime();
+        
+        // insérer test de durée ici
+        
+        fin = System.nanoTime();
+        
+        System.out.println("Cette opération a duré "+(fin-debut));
+        
+          
+              
+    }
+    
+}
