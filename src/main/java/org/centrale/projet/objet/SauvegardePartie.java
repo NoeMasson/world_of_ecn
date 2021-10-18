381286c6ab6e994a4acffb12c578052458b61252
Noe Masson <noemasson@laposte.net>
2021-10-18 15:09:09 +0200

    Added the interface IO and the class SauvegardePartie

---
+++ src/main/java/org/centrale/projet/objet/SauvegardePartie.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.projet.objet;
+import java.io.*;
+/**
+ *
+ * @author Noé Masson
+ * @author Valentin Molina
+ * 
+ * Class allowing to save a game in the World Of ECN project
+ */
+public class SauvegardePartie {
+
+    /**
+     * @return the fileName
+     */
+    public String getFileName() {
+        return fileName;
+    }
+
+    /**
+     * @param fileName the fileName to set
+     */
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * @return the writer
+     */
+    public BufferedWriter getWriter() {
+        return writer;
+    }
+
+    /**
+     * @param writer the writer to set
+     */
+    public void setWriter(BufferedWriter writer) {
+        this.writer = writer;
+    }
+    
+    private String fileName;
+    private BufferedWriter writer;
+    
+    protected SauvegardePartie(){
+        fileName = "testSave.txt";
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    protected SauvegardePartie(String name){
+        fileName = name;
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+}
