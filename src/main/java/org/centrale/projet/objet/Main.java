689ff4194097f7123a1989d7528f0c9fb44bcf64
Valentin <valentin@molina.pro>
2021-11-05 14:22:12 +0100

    Add spacing to main

--- src/main/java/org/centrale/projet/objet/Main.java
+++ src/main/java/org/centrale/projet/objet/Main.java
@@ -21,95 +21,113 @@ public class Main {
         World world = null;
         java.util.Scanner in = new java.util.Scanner(System.in);
         SauvegardePartie save = new SauvegardePartie(); 
-        while(world == null)
-        {
-            System.out.println("Voulez-vous charger une partie ou en commencer une nouvelle ?");
-            System.out.println("1 - Charger une partie.");
-            System.out.println("2 - Nouvelle partie.");
-            System.out.println("");
-            System.out.println("Entrez le numéro correspond à votre choix : ");
-            
-            int choice = 0;
-            while(choice != 1 && choice != 2){
-                try{
-                    choice = Integer.parseInt(in.nextLine());
-                } catch(NumberFormatException ex) {
-                    System.out.println("Vous devez entrer un entier ! Recommencez :");
-                }    
-            }
-            
-            switch(choice){
-                case 1 -> 
-                {
-                    //To test
-                    boolean loaded = false;
-                    
-                    while(!loaded){
-                        
-                    System.out.println("Rentrez le nom du fichier que vous souhaitez charger (sans le .txt de fin)");
-                    String file;
-                    file = in.nextLine();
-                    String fileName = file + ".txt";
-                    
-                    save.setFileName(file);
-                    
-                    try {
-                    ChargementPartie loader = new ChargementPartie(fileName);
-                    world = loader.chargerPartie();
-                    loaded = true;
+        
+        System.out.println("");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.println("                            Bienvenue dans World Of ECN                         ");
+        System.out.println("");
+        System.out.println("                       Un jeu de Noé Masson et Valentin Molina                  ");
+        System.out.println("");
+        System.out.println("                                      2021-2022                                 ");
+        System.out.println("");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.println("Voulez-vous charger une partie ou en commencer une nouvelle ?");
+        System.out.println("1 - Charger une partie.");
+        System.out.println("2 - Nouvelle partie.");
+        System.out.println("");
+        System.out.println("Entrez le numéro correspond à votre choix : ");
 
-                    } catch(FileNotFoundException e){
-                        System.out.println("File not found.");
-                    } catch(IOException ex) {
-                        ex.printStackTrace();
-                    } catch(WrongSaveFileFormatException e){
-                        System.out.println(e.getMessage());
-                    } catch (InstantiationException ex) {
-                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (IllegalAccessException ex) {
-                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (IllegalArgumentException ex) {
-                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (InvocationTargetException ex) {
-                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    }
-                }
-                case 2 -> {
-                    //To test
-                    System.out.println("Voulez-vous donner un nom à votre partie ?");
-                    System.out.println("1 - Oui.");
-                    System.out.println("2 - Non.");
-                    System.out.println("");
-                    System.out.println("Entrez le numéro correspond à votre choix : ");
-                    int choice2 = 0;
-                    while(choice2 != 1 && choice2 != 2){
-                        try{
-                            choice2 = Integer.parseInt(in.nextLine());
-                        } catch(NumberFormatException ex) {
-                        System.out.println("Vous devez entrer un entier ! Recommencez :");
-                        }    
-                    }
-                    switch(choice2){
-                        
-                        case 1 ->{
-                           
-                            System.out.println("Entrez le nom du fichier (sans le .txt de fin):");
-                            String file = in.nextLine() + ".txt";
-                            save.setFileName(file);
-                            }
-                        
-                        case 2 ->{
-                            System.out.println("Le nom de la sauvegarde est : "+ save.getFileName());
-                        }
-                    }
-                }
-            }
-            
-            
+        int choice = 0;
+        while(choice != 1 && choice != 2){
+            try{
+                choice = Integer.parseInt(in.nextLine());
+            } catch(NumberFormatException ex) {
+                System.out.println("Vous devez entrer un entier ! Recommencez :");
+            }    
         }
         
-        int choice = 0;
+        System.out.println("");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("");
+        
+        switch(choice){
+            case 1 -> 
+            {
+                //To test
+                boolean loaded = false;
+
+                while(!loaded){
+
+                System.out.println("Rentrez le nom du fichier que vous souhaitez charger (sans le .txt de fin)");
+                String file;
+                file = in.nextLine();
+                String fileName = file + ".txt";
+
+                save.setFileName(file);
+
+                try {
+                ChargementPartie loader = new ChargementPartie(fileName);
+                world = loader.chargerPartie();
+                loaded = true;
+
+                } catch(FileNotFoundException e){
+                    System.out.println("File not found.");
+                } catch(IOException ex) {
+                    ex.printStackTrace();
+                } catch(WrongSaveFileFormatException e){
+                    System.out.println(e.getMessage());
+                } catch (InstantiationException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalArgumentException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InvocationTargetException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                }
+                
+                System.out.println("");
+                System.out.println("Chargement réussi !");
+            }
+            case 2 -> {
+                //To test
+                System.out.println("Voulez-vous donner un nom à votre partie ?");
+                System.out.println("1 - Oui.");
+                System.out.println("2 - Non.");
+                System.out.println("");
+                System.out.println("Entrez le numéro correspond à votre choix : ");
+                int choice2 = 0;
+                while(choice2 != 1 && choice2 != 2){
+                    try{
+                        choice2 = Integer.parseInt(in.nextLine());
+                    } catch(NumberFormatException ex) {
+                    System.out.println("Vous devez entrer un entier ! Recommencez :");
+                    }    
+                }
+                switch(choice2){
+
+                    case 1 ->{
+
+                        System.out.println("Entrez le nom du fichier (sans le .txt de fin):");
+                        String file = in.nextLine() + ".txt";
+                        save.setFileName(file);
+                        }
+
+                    case 2 ->{
+                        System.out.println("Le nom de la sauvegarde est : "+ save.getFileName());
+                    }
+                }
+            }
+        }
+            
+        System.out.println("");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("");
+        
+        choice = 0;
         while(choice != 4)
         {
             System.out.println("Que voulez-vous faire ?");
@@ -120,7 +138,7 @@ public class Main {
             System.out.println("");
             System.out.println("Entrez le numéro correspond à votre choix : ");
 
-            while(choice != 1 && choice != 2 && choice != 3){
+            while(choice != 1 && choice != 2 && choice != 3 && choice != 4){
                 try{
                     choice = Integer.parseInt(in.nextLine());
                 } catch(NumberFormatException ex) {
@@ -141,10 +159,17 @@ public class Main {
                     save.sauvegarderPartie(world);
                     choice = 4;
                 }
+                case 4 -> {
+                    break;
+                }
             }
+            
+            System.out.println("");
+            System.out.println("--------------------------------------------------------------------------------");
+            System.out.println("");
+            
         }
         
-        System.out.println("");
         System.out.println("Aurevoir.");
         
     }
